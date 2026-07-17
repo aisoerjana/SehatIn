9a1eb7a84c77fa442f628ac55001ba696d90c1b2
aisoerjana <aisoerjana@gmail.com>
2026-07-17 13:41:09 +0700

    chunk size

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,9 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss()],
+    tailwindcss()
+  ],
+  build: {
+    chunkSizeWarningLimit: 600,
+  },
 })
